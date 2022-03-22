28d5662fd32ddfd927eebc532f196f1e375a6c27
omkar patke <omkarpatke1101@gmail.com>
2022-03-22 10:52:31 +0530

    docs : added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import './Footer.css';
+import {  Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <footer className="footer">
+        <div className="footer-links-container">
+            <ul>
+                <h2 className="footer-links-heading">SUPPORT</h2>
+                <li><Link to={{pathname : '#'}}>Warranty</Link></li>
+                <li><Link to={{pathname : '#'}}>Contact Us</Link></li>
+                <li><Link to={{pathname : '#'}}>FAQs</Link></li>
+                <li><Link to={{pathname : '#'}}>Shipping Policy</Link></li>
+                <li><Link to={{pathname : '#'}}>Payments</Link></li>
+                <li><Link to={{pathname : '#'}}>Cancellations & Returns</Link></li>
+            </ul>
+
+            <ul>
+                <h2 className="footer-links-heading">About Us</h2>
+                <li><Link to={{pathname:'#'}}>Corporate Profile</Link></li>
+                <li><Link to={{pathname:'#'}}>Videos</Link></li>
+                <li><Link to={{pathname:'#'}}>Stores</Link></li>
+            </ul>
+
+            <ul>
+                <h2 className="footer-links-heading">Policy</h2>
+                <li><Link to={{pathname:'#'}}>Privacy Policy</Link></li>
+                <li><Link to={{pathname:'#'}}>Terms & Conditions</Link></li>
+            </ul>
+
+            <ul>
+                <h2 className="footer-links-heading">Follow Us</h2>
+                <div className="social-media-links">
+                    <li><Link to={{ pathname:"https://www.facebook.com/omkar.patke.1/" }} target="_blank" className="facebook-link"><i className="lni lni-facebook-original"></i></Link></li>
+                    <li><Link to={{ pathname:"https://twitter.com/omkar_patke" }} className="twitter-link"><i className="lni lni-twitter-original"></i></Link></li>
+                    <li><Link to={{ pathname:"https://www.linkedin.com/in/omkar-patke-a61b221ab/"}} className="linkedIn-link"><i className="lni lni-linkedin-original"></i></Link></li> 
+                </div>
+            </ul>
+        </div>
+
+    </footer>
+    </>
   )
 }
