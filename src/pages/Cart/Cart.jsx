371e28887a59b5e7a2ff132a15364e6f191c31d3
omkar patke <omkarpatke1101@gmail.com>
2022-03-29 20:04:38 +0530

    refactor : remove unwanted dependency

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -64,7 +64,7 @@ export default function Cart() {
             setTotalAmount(totalItemPrice - totalDiscountPrice + deliveryCharges)
         }
         getTotalAmount();
-    },[cartData , increment , decrement ,deliveryCharges ,totalDiscountPrice , totalItemPrice]);
+    },[cartData ,deliveryCharges ,totalDiscountPrice , totalItemPrice]);
 
     
     const removeItemFromCartlist = async(product) => {
