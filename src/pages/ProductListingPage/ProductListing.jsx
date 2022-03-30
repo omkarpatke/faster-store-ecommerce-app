a3a9d433522f84e21fb23b4542e027bb7ccb06dd
omkar patke <omkarpatke1101@gmail.com>
2022-03-30 18:10:15 +0530

    refactor : remove un wanted spaces

--- src/pages/ProductListingPage/ProductListing.jsx
+++ src/pages/ProductListingPage/ProductListing.jsx
@@ -80,7 +80,7 @@ useEffect(() => {
 let filteredData = genderFilterData();
 useEffect(() => {
     setFilterData(filteredData)
-},[ rangeInput , oneStar ,twoStar , threeStar , fourStar , hurculesInput , montraInput , machCityInput , roadeoInput , bsaInput , maleInput ,femaleInput ,lowToHighInput , highToLowInput ,cityBikeInput , mountainBikeInput , kidBikeInput ,setFilterData])
+},[rangeInput , oneStar ,twoStar , threeStar , fourStar , hurculesInput , montraInput , machCityInput , roadeoInput , bsaInput , maleInput ,femaleInput ,lowToHighInput , highToLowInput ,cityBikeInput , mountainBikeInput , kidBikeInput ,setFilterData])
   return (
     <>
     <div className="cycles-main-container">
