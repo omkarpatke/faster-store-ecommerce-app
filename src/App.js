2b9f9510de5fd5065fa420ff7a7d45bb7e9ff203
omkar patke <omkarpatke1101@gmail.com>
2022-03-30 18:06:22 +0530

    refactor : remove br from app.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <br />
       <Routes>
         <Route path='' element={<Home />} />
         <Route path='/products' exact element={<ProductListing />} />
