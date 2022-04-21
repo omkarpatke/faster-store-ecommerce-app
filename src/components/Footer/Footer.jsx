2a0d44fa372034be849b7f1c7e27f3516913b63d
omkar patke <omkarpatke1101@gmail.com>
2022-04-21 11:41:53 +0530

    fix : fix the default function issue

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 import {  Link } from "react-router-dom";
 
-export default function Footer() {
+export function Footer() {
   return (
     <>
     <footer className="footer">
