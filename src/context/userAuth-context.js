14bea381ac1d8a92be558ee89a702e435a86066e
omkar patke <omkarpatke1101@gmail.com>
2022-05-02 16:30:50 +0530

    fix : remove dependencies from useEffect

--- src/context/userAuth-context.js
+++ src/context/userAuth-context.js
@@ -1,5 +1,5 @@
 import { createContext , useState , useContext, useEffect } from "react";
-import { useLocation , useNavigate } from 'react-router-dom';
+
 
 
 const UserContext = createContext('');
@@ -8,18 +8,15 @@ const useUserAuth = () => useContext(UserContext);
 
 
 const UserContextProvider = ({children}) => {
-    const location = useLocation();
-    const navigate = useNavigate();
     const [isLogIn , setIsLogIn] = useState(false);
 
     useEffect(() => {
        if(localStorage.getItem('token')){
            setIsLogIn(true);
-           navigate(location.pathname);
        }else{
            setIsLogIn(false);
        }
-    },[window.reload])
+    },[])
 
     
 
